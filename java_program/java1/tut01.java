9e9b7a38d0fa3617d18155cd696075b556d59ec7
Aayush Patidar <aayushpatidar6262@gmail.com>
2024-07-13 09:59:23 +0530

    basic java programs

---
+++ java_program/java1/tut01.java
@@ -0,0 +1,14 @@
+
+class Test {
+    String name;
+
+    // Test(String name) {
+    // this.name = name;
+    // }
+
+    public static void main(String[] args) {
+        Test t = new Test();
+        t.name = "rahul";
+        System.out.println(t.name);
+    }
+};
